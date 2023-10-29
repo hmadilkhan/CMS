22fb0494d7a014b5886f713dd2317ef626e7d626
hmadilkhan <adilsziu@outlook.com>
2023-10-29 23:02:53 +0500

    Website Route Added

--- routes/web.php
+++ routes/web.php
@@ -34,6 +34,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/dashboard', [App\Http\Controllers\HomeController::class, 'dashboard'])->name('dashboard');
