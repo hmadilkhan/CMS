a0ca377b029fd3198c8075123cc45d9b52dfc030
hmadilkhan <adilsziu@outlook.com>
2024-09-15 11:32:34 +0500

    Set HEader in main layout

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -14,7 +14,7 @@
                     <a class="nav-link dropdown-toggle pulse p-0" href="#" role="button" data-bs-toggle="dropdown" data-bs-display="static">
                         <img class="avatar lg rounded-circle img-thumbnail" src="{{asset('assets/images/profile_av.png')}}" alt="profile">
                     </a>
-                    {{-- <div class="dropdown-menu rounded-lg shadow border-0 dropdown-animation dropdown-menu-end p-0 m-0">
+                    <div class="dropdown-menu rounded-lg shadow border-0 dropdown-animation dropdown-menu-end p-0 m-0">
                         <div class="card border-0 w280">
                             <div class="card-body pb-0">
                                 <div class="d-flex py-1">
@@ -30,16 +30,16 @@
                                 </div>
                             </div>
                             <div class="list-group m-2 ">
-                                <a href="task.html" class="list-group-item list-group-item-action border-0 "><i class="icofont-tasks fs-5 me-3"></i>My Task</a>
-                                <a href="members.html" class="list-group-item list-group-item-action border-0 "><i class="icofont-ui-user-group fs-6 me-3"></i>members</a>
+                                {{-- <a href="task.html" class="list-group-item list-group-item-action border-0 "><i class="icofont-tasks fs-5 me-3"></i>My Task</a>
+                                <a href="members.html" class="list-group-item list-group-item-action border-0 "><i class="icofont-ui-user-group fs-6 me-3"></i>members</a> --}}
                                 <a href="{{route('profile.logout')}}" class="list-group-item list-group-item-action border-0 "><i class="icofont-logout fs-6 me-3"></i>Signout</a>
-                                <div>
+                                {{-- <div>
                                     <hr class="dropdown-divider border-dark">
-                                </div>
-                                <a href="ui-elements/auth-signup.html" class="list-group-item list-group-item-action border-0 "><i class="icofont-contact-add fs-5 me-3"></i>Add personal account</a>
+                                </div> --}}
+                                {{-- <a href="ui-elements/auth-signup.html" class="list-group-item list-group-item-action border-0 "><i class="icofont-contact-add fs-5 me-3"></i>Add personal account</a> --}}
                             </div>
                         </div>
-                    </div> --}}
+                    </div>
                 </div>
             </div>
 
@@ -49,13 +49,13 @@
             </button>
 
             <!-- main menu Search-->
-            {{-- <div class="order-0 col-lg-4 col-md-4 col-sm-12 col-12 mb-3 mb-md-0 ">
-                <div class="input-group flex-nowrap input-group-lg">
+            <div class="order-0 col-lg-4 col-md-4 col-sm-12 col-12 mb-3 mb-md-0 ">
+                {{-- <div class="input-group flex-nowrap input-group-lg">
                     <button type="button" class="input-group-text" id="addon-wrapping"><i class="fa fa-search"></i></button>
                     <input type="search" class="form-control" placeholder="Search" aria-label="search" aria-describedby="addon-wrapping">
                     <button type="button" class="input-group-text add-member-top" id="addon-wrappingone" data-bs-toggle="modal" data-bs-target="#addUser"><i class="fa fa-plus"></i></button>
-                </div>
-            </div> --}}
+                </div> --}}
+            </div>
 
         </div>
     </nav>
