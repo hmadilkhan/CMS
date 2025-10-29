0e464f167b1ad363425e31cbb45e587940fff158
hmadilkhan <adilsziu@outlook.com>
2025-10-29 17:19:00 +0500

    Chanegs

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -146,7 +146,7 @@ class ProjectController extends Controller
         })->sortBy('department_id')->values();
 
 
-        $project = Project::with("task", "customer", "customer.finances", "customer.finances.finance", "department", "logs", "logs.call", "logs.user", "subdepartment", "assignedPerson", "assignedPerson.employee", "departmentnotes", "departmentnotes.user", "salesPartnerUser")
+        $project = Project::with("task", "customer", "customer.finances", "customer.finances.finance", "department", "logs", "logs.call", "logs.user", "subdepartment", "assignedPerson", "assignedPerson.employee", "departmentnotes", "departmentnotes.user", "salesPartnerUser","projectAcceptance")
             ->withCount(['emails as viewed_emails_count' => function ($query) {
                 $query->where('is_view', 1);
             }])
