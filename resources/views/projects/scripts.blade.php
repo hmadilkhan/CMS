548c1e973af86a4d193d5bf3af9f3ac42fed1ff4
hmadilkhan <adilsziu@outlook.com>
2025-10-30 09:11:04 +0500

    Changes revert

--- resources/views/projects/scripts.blade.php
+++ resources/views/projects/scripts.blade.php
@@ -1,9 +1,9 @@
 @section("scripts")
 <script type="text/javascript">
     $('.select2').select2();
-    console.log("Selected Deparrtment","{{selectedDepartment}}");
+    // console.log("Selected Deparrtment","{{selectedDepartment}}");
     
-    projectList("{{selectedDepartment}}");
+    // projectList("{{selectedDepartment}}");
     function projectList(value) {
         let search = $("#search").val();
         $.ajax({
