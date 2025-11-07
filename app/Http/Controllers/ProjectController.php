ba3ad99ed1c1ae1d763ff444556bcc89b810a057
hmadilkhan <adilsziu@outlook.com>
2025-11-07 09:18:48 +0500

    Changes in sub department priority show list

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -720,7 +720,7 @@ class ProjectController extends Controller
             });
         }
         if ($request->id == "all") {
-            $subdepartmentsQuery->groupBy("department_id");
+            $subdepartmentsQuery->groupBy("department_id")->orderBy('order', 'asc');
         }
         if (in_array("Super Admin", auth()->user()->getRoleNames()->toArray())) {
             $departments = Department::with("subdepartments")->where("id", "!=", 9)->get();
