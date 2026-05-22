2aca22a5178bfe663862dbecd023a8f1772ffcb1
hmadilkhan <adilsziu@outlook.com>
2026-05-22 11:27:14 +0500

    Email Format check

--- resources/views/projects/partial/show-emails.blade.php
+++ resources/views/projects/partial/show-emails.blade.php
@@ -20,6 +20,23 @@ return $item->department_id == $department->id;
     <div class="col-lg-12">
         <div class="card">
             @foreach ($emails as $key => $email)
+            @php
+                $rawBody = (string) $email->body;
+                $decodedBody = html_entity_decode(strip_tags($rawBody), ENT_QUOTES | ENT_HTML5, 'UTF-8');
+                $decodedBody = preg_replace("/\r\n|\r/", "\n", $decodedBody);
+                $decodedBody = trim(preg_replace("/\n{3,}/", "\n\n", $decodedBody));
+                $quotePattern = '/(\n\s*On\s.+?wrote:\s*|\n\s*-{2,}\s*Original Message\s*-{2,}|\n\s*From:\s.+\n\s*Sent:\s.+)/is';
+                $bodyParts = preg_split($quotePattern, $decodedBody, 2, PREG_SPLIT_DELIM_CAPTURE);
+                $latestReply = trim($decodedBody);
+                $quotedMessage = '';
+
+                if (count($bodyParts) >= 3) {
+                    $latestReply = trim($bodyParts[0]);
+                    $quotedMessage = trim($bodyParts[1] . $bodyParts[2]);
+                }
+
+                $quotedMessageId = 'quoted-email-' . $email->id;
+            @endphp
             <div class="chat">
                 <div class="chat-history">
                     <ul class="m-b-0">
@@ -47,7 +64,22 @@ return $item->department_id == $department->id;
                                 <span class="message-data-time">{{date("d M Y H:i a", strtotime($email->received_date))}}</span>
                             </div>
                             <div class="message my-message">
-                                {{ $email->subject }}</br>{{ $email->body }}</br>
+                                <p class="text-start fw-bold mb-2">{{ $email->subject }}</p>
+                                <div class="text-start bg-white rounded p-3 border">
+                                    {!! nl2br(e($latestReply)) !!}
+                                </div>
+                                @if($quotedMessage !== '')
+                                <button class="btn btn-sm btn-outline-secondary mt-2" type="button"
+                                    data-bs-toggle="collapse" data-bs-target="#{{ $quotedMessageId }}"
+                                    aria-expanded="false" aria-controls="{{ $quotedMessageId }}">
+                                    <i class="icofont-rounded-down me-1"></i>Show original message
+                                </button>
+                                <div class="collapse mt-2" id="{{ $quotedMessageId }}">
+                                    <div class="text-start bg-light rounded p-3 border text-muted small">
+                                        {!! nl2br(e($quotedMessage)) !!}
+                                    </div>
+                                </div>
+                                @endif
                                 @if(!empty($email->attachments))
                                 @foreach($email->attachments as $attachment)
                                 <a target="_blank" href="{{asset('/storage/emails/'.$attachment->file)}}"><span class="badge bg-primary">{{$attachment->file}}</span></a>
